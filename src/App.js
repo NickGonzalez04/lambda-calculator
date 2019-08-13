21e172e8982077ba4ac7ff153a2f31d0722ccdad
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-08-13 19:08:10 -0400

    Continued CSS styling

--- src/App.js
+++ src/App.js
@@ -40,9 +40,9 @@ const addOperator = (operator) => {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
      <Display number={displayValue} />
+     <Specials />
      <Numbers addNumber={addNumber} />
      <Operators addOperator={addOperator}/>
-     <Specials />
       </div>
     </div>
   );
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const SpecialButton = (props) => {
   console.log("special",props);
   return (
-    <button>
+    <button className="special-button">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       {props.text}
     </button>
