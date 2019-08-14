d49af37e71b8fff4e148887b11cdd5f8ddeaa597
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-08-14 14:58:01 -0400

    Finsihed MVP

--- src/App.js
+++ src/App.js
@@ -39,10 +39,15 @@ const addOperator = (operator) => {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+
      <Display number={displayValue} />
-     <Specials />
-     <Numbers addNumber={addNumber} />
-     <Operators addOperator={addOperator}/>
+      <div className="button-container">
+        <div className="specials-container">
+          <Specials />
+          <Numbers addNumber={addNumber} />
+            </div>
+           <Operators addOperator={addOperator}/>
+     </div>
       </div>
     </div>
   );
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -8,7 +8,7 @@ const Specials = (props) => {
   // STEP 2 - add the imported data to state
 
   return (
-    <div>
+    <div className="specials-contianer">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
